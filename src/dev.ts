d5e8ee277c0324111a93cf6179582d667f596c3f
Thanaen <thanaen.dev@pm.me>
2026-03-28 19:03:08 +0100

    build: add build step for vercel deploy

---
+++ src/dev.ts
@@ -0,0 +1,4 @@
+import app from "./index";
+app.listen(3000, () => {
+  console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { Elysia } from "elysia";
 
 import { paniers } from "./modules/paniers";
 
-const app = new Elysia()
-  .use(paniers)
-  .get("/", ({ redirect }) => redirect("https://thanaen.dev"))
-  .listen(3000);
+const app = new Elysia().use(paniers).get("/", ({ redirect }) => redirect("https://thanaen.dev"));
 
-console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+export default app;
